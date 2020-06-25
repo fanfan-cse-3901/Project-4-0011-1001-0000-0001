11868ee544c98212cd51572bae4043aa7990d148
Yifan Yao <yao.740@osu.edu>
2020-06-25 19:17:25 -0400

    Update publications.rb Add support for <i> tags Update publications.html.erb Syntax error fixed
    Pass: HTML Validator, CSS Validator, htmllint, stylelint

--- data_bridge/publications.rb
+++ data_bridge/publications.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# File created 06/25/2020 by Yifan Yao
+# Scrapping form http://web.cse.ohio-state.edu/~davis.1719/publications.html and divide it equally to two files
+#
+# Output data/event_list_left.json, data/event_list_right.json
+
 require 'mechanize'
 require 'json'
 
@@ -9,7 +14,7 @@ page = agent.get 'http://web.cse.ohio-state.edu/~davis.1719/publications.html'
 # get content form <li> tags
 event_list = []
 page.search('//ul/li').each do |i|
-  # Reject [HTML], [PDF], link into item
+  # reject [HTML], [PDF], link into item
   item = i.text.split("\n").reject do |c|
     c.empty? ||
       c.match(/(HTML)|(PDF)/) ||
@@ -17,26 +22,34 @@ page.search('//ul/li').each do |i|
       c.match(%r{(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)})
   end
 
-  # Add actual link into item
+  # add actual link into item
   i.search('a').each do |link|
     item << link['href']
+  end
+
+  # get text between <i> tags
+  italic_str = ''
+  i.search('i').each do |italic|
+    italic_str = italic.text
+  end
+
+  item.each do |str|
+    if italic_str != '' &&
+        !str.match(%r{https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)}) &&
+        !str.match(%r{Publications/})
+      str.replace(str.gsub italic_str, "<i>#{italic_str}</i>")
+    end
   end
 
   # put item into event_list
   event_list << item
 end
 
-# get content form <i> tags
-list_italic = []
-page.search('//ul/li/i').each do |i|
-  list_italic << i.text unless list_italic.include? i.text
-end
-
 # divide event_list equally
 event_list_left, event_list_right = event_list.each_slice((event_list.size / 2.0).round).to_a
 
-def json_output(arr, file_name)
-  file = File.open "./data/#{file_name}.json", 'w' do |line|
+def json_output arr, file_name
+  File.open "./data/#{file_name}.json", 'w' do |line|
     line.puts arr.to_json
   end
 end
@@ -44,4 +57,3 @@ end
 # output array to file
 json_output event_list_left, 'event_list_left'
 json_output event_list_right, 'event_list_right'
-json_output list_italic, 'list_italic'
